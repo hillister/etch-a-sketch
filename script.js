9a796baab421170bd39b584766be653536824083
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2024-09-28 21:08:39 +0100

    added 16x16 divs

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const container = document.body.appendChild(document.createElement("div"));
+
+for (i=0; i<16; i++){
+    const row = document.createElement("div")
+    row.classList.add("row");
+    container.appendChild(row);
+    for (j=0; j<16; j++){
+        const column = document.createElement("div")
+        column.classList.add("column")
+        container.appendChild(column); 
+    }
+}
